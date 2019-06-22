ae45cbcd6a894fc613aa32e617bd3de9801a4626
SergeyProger <kogutsergey1@gmail.com>
2019-06-22 17:18:18 +0300

    [#166491964] Create new website HTML/CSS website

---
+++ app/controllers/em_website_controller.rb
@@ -0,0 +1,4 @@
+class EmWebsiteController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/em_website_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EmWebsiteControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
