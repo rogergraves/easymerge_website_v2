5e817d41c558e5676881ceb2f3120dc426f57b3d
SergeyProger <kogutsergey1@gmail.com>
2019-06-25 17:52:06 +0300

    [#166491964] I am working on the page - how it works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'em_website#index'
-
+  get 'how_it_works', to: 'em_website#how_it_works'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
