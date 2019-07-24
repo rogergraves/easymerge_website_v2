9071dca6bdfebcbc240928e5a6825b1a0f1d0eae
SergeyProger <kogutsergey1@gmail.com>
2019-07-24 17:03:58 +0300

    [#167387570] Fade in quotes when user scrolls down to them.

--- app/assets/javascripts/em_website.js
+++ app/assets/javascripts/em_website.js
@@ -1,2 +1,27 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+
+var quoteAnimation = function() {
+    $(".method").sort(function(){
+        return Math.random() - 0.5;
+    }).each(function(i,elem) {
+        $(elem).delay(i*1000).animate({
+            opacity: 1
+        }, "slow"
+        );
+    });
+};
+
+$(window).scroll(function() {
+    if ($(window).width() <= 767) {
+        if ($(window).scrollTop() > 3300) {
+            quoteAnimation();
+        }
+    } else if ($(window).width() > 767 && $(window).width() <= 991) {
+        if ($(window).scrollTop() > 2350) {
+            quoteAnimation();
+        }
+    } else {
+        if ($(window).scrollTop() > 2000) {
+            quoteAnimation();
+        }
+    }
+});
